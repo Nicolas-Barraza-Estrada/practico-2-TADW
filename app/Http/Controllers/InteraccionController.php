6ba8b691fa53641d39eee531b4c1fa2daf6b1183
Nicolas-Barraza-Estrada <nicolas.barraza1901@alumnos.ubiobio.cl>
2023-11-28 09:41:00 -0300

    ..

--- app/Http/Controllers/InteraccionController.php
+++ app/Http/Controllers/InteraccionController.php
@@ -24,20 +24,18 @@ class InteraccionController extends Controller
         // Verificar si hay un match
         $hayMatch = $this->verificarMatch($interaccion);
 
-        if ($hayMatch) {
+        if (!$hayMatch) {
             return response()->json(['message' => '¡Hay match!']);
+        }else{
+            return response()->json(['message' => 'OK']);
         }
-
-        return response()->json(['message' => 'OK']);
     }
 
     private function verificarMatch($interaccion)
-    {
+    {   
         // Obtener información sobre la interacción
         $perroInteresadoId = $interaccion->perro_interesado_id;
         $perroCandidatoId = $interaccion->perro_candidato_id;
-        $preferencia = $interaccion->preferencia;
-
         // Verificar si la interacción es mutua (match)
         $interaccionMutua = Interaccion::where('perro_interesado_id', $perroCandidatoId)
             ->where('perro_candidato_id', $perroInteresadoId)
